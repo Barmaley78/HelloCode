400db4bc840782db2069b136d3d8658d8a883e4b
Sergey <barmaley78@mail.ru>
2022-10-12 00:35:40 +0700

    created all examples

---
+++ ex5/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите имя пользователя: ");
+string username = ReaderWriterLockSlim.Line();
+if (username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура, это же Маша");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
