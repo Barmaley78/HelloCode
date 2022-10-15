cbdbbc84ac0f89adb18825ba7d5ca54e91f87063
Sergey <barmaley78@mail.ru>
2022-10-15 17:05:37 +0700

    первые методы

---
+++ ex16/Program.cs
@@ -0,0 +1,47 @@
+﻿void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while(index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while(position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int pos = -1;
+    while(index < count)
+    {
+        if(collection[index] == find)
+        {
+            pos = index;
+            break;
+        }
+        index++;
+    }
+    return pos;
+}
+
+
+int[] ar = new int [10];
+
+FillArray(ar);
+PrintArray(ar);
+Console.WriteLine();
+int pos = IndexOf(ar, 4);
+Console.WriteLine(pos);
