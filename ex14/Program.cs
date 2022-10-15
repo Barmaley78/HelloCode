7e07f0ab6d7fb3803eb35e10fb3cb443f85a8cad
Sergey <barmaley78@mail.ru>
2022-10-15 16:46:37 +0700

    первая задача с массивом

---
+++ ex14/Program.cs
@@ -0,0 +1,18 @@
+﻿// нахождение максимума из 9 чисел методом
+int Max(int arg1, int arg2, int arg3)
+{
+    int res = arg1;
+    if (res < arg2) res = arg2;
+    if (res < arg3) res = arg3;
+    return res;
+}
+
+int [] array = {11,21,31,41,15,61,17,18,19};
+
+int max = Max(
+    Max(array[0], array[1], array[2]), 
+    Max(array[3], array[4], array[5]), 
+    Max(array[6], array[7], array[8])
+    );
+
+Console.WriteLine(max);
---
+++ ex15/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] ar = { 123, 435, 678, 23, 23, 687, 13, 356 };
+
+int n = ar.Length;
+int find = 23;
+int index = 0;
+
+while(index < n)
+{
+    if(ar[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
